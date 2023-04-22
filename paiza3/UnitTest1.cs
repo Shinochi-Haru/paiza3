474ed2a58d5a7bd8c0fc5e055548178199600f6e
Shinochi-Haru <haru060109011305@gmail.com>
2023-04-22 22:39:15 +0900

    修正

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -1,39 +1,29 @@
 using System;
 using System.Collections.Generic;
 
-class MainClass
+class Program
 {
-    static List<char> stack = new List<char>();     // スタック本体
-    static int stack_size = 0;  // スタックに入っている要素数
-
-    public static void Main(string[] args)
+    static void Main()
     {
         int q = int.Parse(Console.ReadLine());
+        Queue<char> queue = new Queue<char>();
         for (int i = 0; i < q; i++)
         {
-            string[] queryLine = Console.ReadLine().Split(' ');
-            int query = int.Parse(queryLine[0]);
-            if (query == 1)
+            string[] query = Console.ReadLine().Split();
+            if (query[0] == "1")
             {
                 // PUSH
-                char x = char.Parse(queryLine[1]);
-                stack.Add(x);
-                stack_size++;
+                char x = char.Parse(query[1]);
+                queue.Enqueue(x);
             }
-            else if (query == 2)
+            else if (query[0] == "2")
             {
                 // POP
-                //stack_size--;
-                //Console.WriteLine(stack[stack_size]);
-                stack.RemoveAt(0);
+                Console.WriteLine(queue.Dequeue());
             }
-            for (int j = 0; j < stack_size; j++)
+            foreach (char c in queue)
             {
-                if (j > 0)
-                {
-                    Console.Write(" ");
-                }
-                Console.Write(stack[j]);
+                Console.Write(c + " ");
             }
             Console.WriteLine();
         }
