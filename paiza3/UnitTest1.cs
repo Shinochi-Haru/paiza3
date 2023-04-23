fd894f04c99d865ab5d310de9a0804f0ea298f5a
Shinochi-Haru <haru060109011305@gmail.com>
2023-04-23 22:11:46 +0900

    修正

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -6,43 +6,27 @@ class Program
     static void Main()
     {
         int q = int.Parse(Console.ReadLine());
-        Queue<int> queue1 = new Queue<int>();
-        Queue<int> queue2 = new Queue<int>();
+        Queue<char> queue = new Queue<char>();
         for (int i = 0; i < q; i++)
         {
             string[] query = Console.ReadLine().Split();
             if (query[0] == "1")
             {
                 // PUSH
-                int k = int.Parse(query[1]);
-                int x = int.Parse(query[2]);
-                if (k == 1)
-                {
-                    queue1.Enqueue(x);
-                }
-                else if (k == 2)
-                {
-                    queue2.Enqueue(x);
-                }
+                char x = char.Parse(query[1]);
+                queue.Enqueue(x);
             }
             else if (query[0] == "2")
             {
                 // POP
-                int k = int.Parse(query[1]);
-                if (k == 1)
-                {
-                    Console.WriteLine(queue1.Dequeue());
-                }
-                else if (k == 2)
-                {
-                    Console.WriteLine(queue2.Dequeue());
-                }
+                Console.WriteLine(queue.Dequeue());
             }
-            else if (query[0] == "3")
+
+            foreach (char c in queue)
             {
-                // SIZE
-                Console.WriteLine(queue1.Count + " " + queue2.Count);
+                Console.Write(c + " ");
             }
+            Console.WriteLine();
         }
     }
 }
