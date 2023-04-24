4fbb0735299521c99451d8564e29eb3f5b214fa9
Shinochi-Haru <haru060109011305@gmail.com>
2023-04-24 20:08:21 +0900

    修正

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -1,38 +1,29 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        int n = int.Parse(Console.ReadLine());
+        int num = int.Parse(Console.ReadLine());
+        var nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
         Stack<int> st = new Stack<int>();
-        for (int i = 0; i < n; i++)
+
+        for(int i = 0; i < 0; i++)
         {
-            int a = int.Parse(Console.ReadLine());
-            st.Push(a);
-            while (st.Count >= 2)
+            st.Push(nums[i]);
+            while(st.Count >= 2)
             {
-                int top1 = st.Peek(); // スタックの上から一番目
+                int top1 = st.Peek();
                 st.Pop();
-                int top2 = st.Peek(); // スタックの上から二番目
+                int top2 = st.Peek();
                 st.Pop();
                 if (top1 == top2)
                 {
-                    st.Push(2 * top2);
-                }
-                else
-                {
-                    st.Push(top2);
-                    st.Push(top1);
-                    break;
+                    
                 }
             }
-        }
-        while (st.Count > 0)
-        {
-            Console.WriteLine(st.Peek());
-            st.Pop();
         }
     }
 }
