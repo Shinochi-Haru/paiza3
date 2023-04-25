8e8e4671eb1ba69bb2aab2cdd59382e9e95314ee
Shinochi-Haru <haru060109011305@gmail.com>
2023-04-25 21:29:24 +0900

    修正

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -1,29 +1,32 @@
 using System;
-using System.Linq;
 using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        int num = int.Parse(Console.ReadLine());
-        var nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
-        Stack<int> st = new Stack<int>();
-
-        for(int i = 0; i < 0; i++)
+        int q = int.Parse(Console.ReadLine());
+        Queue<char> queue = new Queue<char>();
+        for (int i = 0; i < q; i++)
         {
-            st.Push(nums[i]);
-            while(st.Count >= 2)
+            string[] query = Console.ReadLine().Split();
+            if (query[0] == "1")
             {
-                int top1 = st.Peek();
-                st.Pop();
-                int top2 = st.Peek();
-                st.Pop();
-                if (top1 == top2)
-                {
-                    
-                }
+                // PUSH
+                char x = char.Parse(query[1]);
+                queue.Enqueue(x);
             }
+            else if (query[0] == "2")
+            {
+                // POP
+                Console.WriteLine(queue.Dequeue());
+            }
+
+            foreach (char c in queue)
+            {
+                Console.Write(String.Join(" " , c));
+            }
+            Console.WriteLine();
         }
     }
 }
