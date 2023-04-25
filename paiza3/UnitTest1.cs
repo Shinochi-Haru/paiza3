1c03d85e154439add42a5ee8ef60ae2018e763e2
Shinochi-Haru <haru060109011305@gmail.com>
2023-04-25 21:50:41 +0900

    修正

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -1,38 +1,25 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 class Program
 {
     static void Main()
     {
-        int q = int.Parse(Console.ReadLine());
-        Queue<char> queue = new Queue<char>();
-        for (int i = 0; i < q; i++)
-        {
-            string[] query = Console.ReadLine().Split();
-            if (query[0] == "1")
-            {
-                // PUSH
-                char x = char.Parse(query[1]);
-                queue.Enqueue(x);
-            }
-            else if (query[0] == "2")
-            {
-                // POP
-                Console.WriteLine(queue.Dequeue());
-            }
+        var num = int.Parse(Console.ReadLine());
+        Stack<int> numsStack = new Stack<int>();
+        Console.WriteLine(num);
 
-            // 連結用のStringBuilderを用意する
-            StringBuilder sb = new StringBuilder();
-            foreach (char c in queue)
-            {
-                // 文字列を連結する
-                sb.Append(c);
-                sb.Append(' ');
-            }
-            // 最後に空白を除去して、文字列に変換して出力する
-            Console.WriteLine(sb.ToString().TrimEnd());
+        for (int i = 0; i < num; i++)
+        {
+            int numValue = int.Parse(Console.ReadLine());
+            numsStack.Push(numValue);
+            numValue = numsStack.Pop();
+            Console.WriteLine(numValue);
         }
+
+        //while (numsStack.Count > 0)
+        //{
+            
+        //}
     }
 }
