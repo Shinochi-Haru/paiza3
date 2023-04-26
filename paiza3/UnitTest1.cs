29abfd7da5e57c33d79579213cc23d14dd7296e7
Shinochi-Haru <haru060109011305@gmail.com>
2023-04-26 17:02:55 +0900

    修正

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -1,25 +1,38 @@
 using System;
 
-class Program
+namespace ƒVƒmƒ`ƒnƒ‹
 {
-    static void Main(string[] args)
+    class Program
     {
-        int[] numArray = new int[] { 7, 1, 3, 4, 2 };
-        for (int i = 1; i < numArray.Length; i++)
+        static void Main(string[] args)
         {
-            int sort = numArray[i];
-            int j = i - 1;
-            while (j >= 0 && numArray[j] > sort)
+            int[] numArray = new int[] { 7, 1, 3, 4, 2 };
+            InsertionSort(numArray, numArray.Length);
+            for (int i = 0; i < numArray.Length; i++)
             {
-                numArray[j + 1] = numArray[j];
-                j--;
+                Console.WriteLine(numArray[i]);
             }
-            numArray[j + 1] = sort;
         }
 
-        for (int i = 0; i < numArray.Length; i++)
+        static void InsertionSort(int[] sortArray, int n)
         {
-            Console.WriteLine(numArray[i]);
+            if (n <= 1)
+            {
+                return;
+            }
+
+            InsertionSort(sortArray, n - 1);
+
+            int sort = sortArray[n - 1];
+            int j = n - 2;
+
+            while (j >= 0 && sortArray[j] > sort)
+            {
+                sortArray[j + 1] = sortArray[j];
+                j--;
+            }
+
+            sortArray[j + 1] = sort;
         }
     }
 }
