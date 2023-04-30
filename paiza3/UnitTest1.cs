8576299602aa471d632535cef9ac0e69d98fb1de
Shinochi-Haru <haru060109011305@gmail.com>
2023-04-30 21:25:53 +0900

    修正

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -4,9 +4,27 @@ class Program
 {
     static void Main()
     {
-        var num = int.Parse(Console.ReadLine());
+        int n, a = 2, fermat = 1;
+        bool is_prime = true;
+        n = int.Parse(Console.ReadLine());
 
-        if (IsPrime(num, 5))
+        if (n % a == 0)
+        {
+            is_prime = false;
+        }
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            fermat *= a;
+            fermat %= n;
+        }
+
+        if (fermat % n != 1)
+        {
+            is_prime = false;
+        }
+
+        if (is_prime)
         {
             Console.WriteLine("YES");
         }
@@ -14,63 +32,5 @@ class Program
         {
             Console.WriteLine("NO");
         }
-    }
-
-    static bool IsPrime(int num, int k)
-    {
-        if (num <= 1)
-        {
-            return false;
-        }
-
-        if (num == 2 || num == 3)
-        {
-            return true;
-        }
-
-        if (num % 2 == 0)
-        {
-            return false;
-        }
-
-        Random rnd = new Random();
-        for (int i = 0; i < k; i++)
-        {
-            int a = rnd.Next(2, num - 2);
-            if (Gcd(a, num) != 1 || ModPow(a, num - 1, num) != 1)
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    static int Gcd(int a, int b)
-    {
-        while (b != 0)
-        {
-            int r = a % b;
-            a = b;
-            b = r;
-        }
-
-        return a;
-    }
-
-    static int ModPow(int x, int y, int m)
-    {
-        int res = 1;
-        while (y > 0)
-        {
-            if ((y & 1) != 0)
-            {
-                res = (res * x) % m;
-            }
-            x = (x * x) % m;
-            y >>= 1;
-        }
-
-        return res;
     }
 }
