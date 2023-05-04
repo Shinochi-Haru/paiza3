0826cc298bb17575fa289f8bb28993d2f0618aa0
Shinochi-Haru <haru060109011305@gmail.com>
2023-05-04 22:22:46 +0900

    修正

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -1,49 +1,25 @@
 using System;
-using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        Console.Write("Enter an integer: ");
         int n = int.Parse(Console.ReadLine());
-
-        int ans = 1;
-        Dictionary<int, int> divisors = new Dictionary<int, int>();
-
-        for (int i = 2; i * i <= n; i++)
+        int[] intArray = new int[n];
+        int factor = 2;
+        for(int i = 0; i < n; i++)
         {
-            while (n % i == 0)
+            intArray[i] = int.Parse(Console.ReadLine());
+            if (intArray[i] % factor == 0)
             {
-                if (divisors.ContainsKey(i))
-                {
-                    divisors[i]++;
-                }
-                else
-                {
-                    divisors.Add(i, 1);
-                }
-                n /= i;
-            }
-        }
-
-        if (n != 1)
-        {
-            if (divisors.ContainsKey(n))
-            {
-                divisors[n]++;
+                Console.Write($"{factor} ");
+                n /= factor;
             }
             else
             {
-                divisors.Add(n, 1);
+                factor++;
             }
         }
-
-        foreach (KeyValuePair<int, int> kvp in divisors)
-        {
-            ans *= kvp.Value + 1;
-        }
-
-        Console.WriteLine($"Number of divisors of {n} is {ans}");
+        Console.WriteLine();
     }
 }
