a070006a846c1e5ceb8cb918d3b6d2a0809987e5
Shinochi-Haru <haru060109011305@gmail.com>
2023-05-04 21:36:32 +0900

    修正

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -1,47 +1,26 @@
 using System;
+
 class Program
 {
     static void Main()
     {
-        var num1 = int.Parse(Console.ReadLine());
-        for (int i = 0; i < num1; i++)
-        {
-            var num = int.Parse(Console.ReadLine());
+        Console.Write("Enter an integer: ");
+        int n = int.Parse(Console.ReadLine());
 
-            if (IsPrime(num))
+        Console.Write($"Prime factors of {n} are: ");
+        int factor = 2;
+        while (n > 1)
+        {
+            if (n % factor == 0)
             {
-                Console.WriteLine("pass");
+                Console.Write($"{factor} ");
+                n /= factor;
             }
             else
             {
-                Console.WriteLine("failure");
+                factor++;
             }
         }
-    }
-
-    static bool IsPrime(int num)
-    {
-        if (num <= 1)
-        {
-            return false;
-        }
-        if (num <= 3)
-        {
-            return true;
-        }
-        if (num % 2 == 0 || num % 3 == 0)
-        {
-            return false;
-        }
-        int i = 5;
-        while (i * i <= num)
-        {
-            if (num % i == 0 || num % (i + 2) == 0)
-            {
-                return false;
-            }
-            i += 6;
-        }
-        return true;
+        Console.WriteLine();
     }
 }
