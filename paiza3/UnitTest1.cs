e1b1b04c993563ed1133eff6c3d7774a14fd388e
Shinochi-Haru <haru060109011305@gmail.com>
2023-05-07 22:37:05 +0900

    修正

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -1,19 +1,21 @@
 using System;
+using System.Linq;
 
 class MainClass
 {
     public static void Main(string[] args)
     {
-        long m1, m2, b1, b2;
-        m1 = long.Parse(Console.ReadLine());
-        m2 = long.Parse(Console.ReadLine());
-        b1 = long.Parse(Console.ReadLine());
-        b2 = long.Parse(Console.ReadLine());
+        //long m1, m2, b1, b2;
+        //m1 = long.Parse(Console.ReadLine());
+        //m2 = long.Parse(Console.ReadLine());
+        //b1 = long.Parse(Console.ReadLine());
+        //b2 = long.Parse(Console.ReadLine());
+        var longArray = Console.ReadLine().Split().Select(long.Parse).ToArray();
 
-        for (long i = 0; i < m2; i++)
+        for (long i = 0; i < longArray[1]; i++)
         {
-            long cand = m1 * i + b1;
-            if (cand % m2 == b2)
+            long cand = longArray[0] * i + longArray[2];
+            if (cand % longArray[1] == longArray[3])
             {
                 Console.WriteLine(cand);
                 return;
