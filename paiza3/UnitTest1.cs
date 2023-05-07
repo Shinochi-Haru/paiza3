e5b287c3f5914563ee4066ee39c9588a2c4b4dca
Shinochi-Haru <haru060109011305@gmail.com>
2023-05-07 22:31:52 +0900

    修正

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -1,59 +1,23 @@
 using System;
-using System.Collections.Generic;
 
 class MainClass
 {
     public static void Main(string[] args)
     {
-        int n, ans1 = 0, ans2 = 0;
-        long multi = 0;
-        n = int.Parse(Console.ReadLine());
+        long m1, m2, b1, b2;
+        m1 = long.Parse(Console.ReadLine());
+        m2 = long.Parse(Console.ReadLine());
+        b1 = long.Parse(Console.ReadLine());
+        b2 = long.Parse(Console.ReadLine());
 
-        bool[] is_prime = new bool[n + 1];
-        List<long> primes = new List<long>();
-
-        for (int i = 0; i <= n; i++)
+        for (long i = 0; i < m2; i++)
         {
-            is_prime[i] = true;
-        }
-        is_prime[0] = false;
-        is_prime[1] = false;
-
-        for (int i = 2; i <= n; i++)
-        {
-            if (is_prime[i])
+            long cand = m1 * i + b1;
+            if (cand % m2 == b2)
             {
-                primes.Add(i);
-                for (int j = i * 2; j <= n; j += i)
-                {
-                    is_prime[j] = false;
-                }
+                Console.WriteLine(cand);
+                return;
             }
-        }
-
-        foreach (long p in primes)
-        {
-            long other_prime = n - p;
-            if (is_prime[other_prime])
-            {
-                if (multi < p * other_prime)
-                {
-                    multi = p * other_prime;
-                    ans1 = (int)p;
-                    ans2 = (int)other_prime;
-                }
-            }
-        }
-
-        if (ans1 <= ans2)
-        {
-            Console.WriteLine(ans1);
-            Console.WriteLine(ans2);
-        }
-        else
-        {
-            Console.WriteLine(ans2);
-            Console.WriteLine(ans1);
         }
     }
 }
