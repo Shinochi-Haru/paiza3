097d87aaa289738db4464c7cc77487f51887797c
Shinochi-Haru <haru060109011305@gmail.com>
2023-05-14 21:55:32 +0900

    修正

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -2,29 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-class MainClass
+class Program
 {
-    public static void Main(string[] args)
+    static void Main()
     {
         int N = int.Parse(Console.ReadLine());
 
-        // チャットに参加したメンバーを保持するリスト
         List<string> membership = new List<string>();
-        // スーパーチャットの情報を保持するリスト
-        List<Tuple<long, string>> superchat = new List<Tuple<long, string>>();
-        // メンバーごとの受け取った金額の合計を保持する辞書
         Dictionary<string, long> money_sum = new Dictionary<string, long>();
 
-        // 入力を受け取り、処理を行う
         for (int i = 0; i < N; i++)
         {
-            string[] input = Console.ReadLine().Split();
+            string[] input = Console.ReadLine().Split(' ');
             string name = input[0];
             string E = input[1];
 
             if (E == "give")
             {
-                // "give"の場合は、受け取った金額を加算する
                 long money = long.Parse(input[2]);
                 if (money_sum.ContainsKey(name))
                 {
@@ -37,41 +31,20 @@ class MainClass
             }
             else
             {
-                // "join"の場合は、チャットに参加したメンバーとして登録する
                 membership.Add(name);
             }
         }
 
-        // スーパーチャットの情報を作成する
-        foreach (var item in money_sum)
+        List<KeyValuePair<string, long>> sorted_sum = money_sum.OrderByDescending(x => x.Value).ToList();
+
+        foreach (KeyValuePair<string, long> pair in sorted_sum)
         {
-            superchat.Add(Tuple.Create(item.Value, item.Key));
+            Console.WriteLine(pair.Key);
         }
 
-        // スーパーチャットの情報を並び替える（金額が多い順、同じ場合は名前の昇順）
-        superchat.Sort((x, y) =>
+        foreach (string name in membership.OrderBy(x => x))
         {
-            int result = y.Item1.CompareTo(x.Item1);
-            if (result == 0)
-            {
-                result = x.Item2.CompareTo(y.Item2);
-            }
-            return result;
-        });
-
-        // チャットに参加したメンバーを並び替える（名前の昇順）
-        membership.Sort();
-
-        // スーパーチャットの情報を出力する
-        foreach (var item in superchat)
-        {
-            Console.WriteLine(item.Item2);
-        }
-
-        // チャットに参加したメンバーを出力する
-        foreach (var item in membership)
-        {
-            Console.WriteLine(item);
+            Console.WriteLine(name);
         }
     }
 }
