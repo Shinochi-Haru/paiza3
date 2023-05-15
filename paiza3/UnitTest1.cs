2bd1cd8e713cc88be16cd9574ab73f5fd74e4c0d
Shinochi-Haru <haru060109011305@gmail.com>
2023-05-15 11:53:48 +0900

    修正

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -1,50 +1,21 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-
 class Program
 {
     static void Main()
     {
-        int N = int.Parse(Console.ReadLine());
+        int num = int.Parse(Console.ReadLine());
 
-        List<string> membership = new List<string>();
-        Dictionary<string, long> money_sum = new Dictionary<string, long>();
-
-        for (int i = 0; i < N; i++)
+        if (num >= 1 && num <= 5)
         {
-            string[] input = Console.ReadLine().Split(' ');
-            string name = input[0];
-            string E = input[1];
-
-            if (E == "give")
-            {
-                long money = long.Parse(input[2]);
-                if (money_sum.ContainsKey(name))
-                {
-                    money_sum[name] += money;
-                }
-                else
-                {
-                    money_sum[name] = money;
-                }
-            }
-            else
-            {
-                membership.Add(name);
-            }
+            Console.WriteLine("raw");
         }
-
-        List<KeyValuePair<string, long>> sorted_sum = money_sum.OrderByDescending(x => x.Value).ToList();
-
-        foreach (KeyValuePair<string, long> pair in sorted_sum)
+        else if (num >= 6 && num <= 7)
         {
-            Console.WriteLine(pair.Key);
+            Console.WriteLine("soft boiled");
         }
-
-        foreach (string name in membership.OrderBy(x => x))
+        else
         {
-            Console.WriteLine(name);
+            Console.WriteLine("hard boiled");
         }
     }
 }
