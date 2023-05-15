552f79ec9ae64f03983714cb9a32c2145d9e8308
Shinochi-Haru <haru060109011305@gmail.com>
2023-05-15 15:29:27 +0900

    修正

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 class Program
 {
@@ -8,18 +7,22 @@ class Program
     {
         var num = int.Parse(Console.ReadLine());
         List<string> data = new List<string>();
+        HashSet<string> names = new HashSet<string>();
 
         for (int i = 0; i < num; i++)
         {
-            var names = Console.ReadLine();
-            if(data.Contains(names))
+            string name = Console.ReadLine();
+
+            if (names.Contains(name))
             {
                 data.Add("NO");
             }
             else
             {
                 data.Add("YES");
+                names.Add(name);
             }
+
             Console.WriteLine(data[i]);
         }
     }
