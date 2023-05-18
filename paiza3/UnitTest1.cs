a085b91870d21186d653f8b4b1f8596c329d64ce
Shinochi-Haru <haru060109011305@gmail.com>
2023-05-18 23:28:44 +0900

    ユークリッド

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -1,79 +1,24 @@
 using System;
-using System.Collections.Generic;
 
-class Program
+public class Program
 {
-    static void Main()
+    public static int EuclideanAlgorithm(int a, int b)
     {
-        string[] input = Console.ReadLine().Split();
-        int n = int.Parse(input[0]);
-        int m = int.Parse(input[1]);
+        while (b != 0)
+        {
+            int remainder = a % b;
+            a = b;
+            b = remainder;
+        }
+        return a;
+    }
 
-        string[] pixels = new string[n];
-        for (int i = 0; i < n; i++)
-        {
-            pixels[i] = Console.ReadLine();
-        }
+    public static void Main(string[] args)
+    {
+        int num1 = 48;
+        int num2 = 36;
+        int gcd = EuclideanAlgorithm(num1, num2);
 
-        // ‚’¼‚ÉÜ‚è‚½‚½‚ÞÛ
-        bool isLineSymmetry1 = true;
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                if (pixels[i][j] != pixels[i][m - j - 1])
-                {
-                    isLineSymmetry1 = false;
-                    break;
-                }
-            }
-        }
-
-        // …•½‚ÉÜ‚è‚½‚½‚ÞÛ
-        bool isLineSymmetry2 = true;
-        for (int j = 0; j < m; j++)
-        {
-            for (int i = 0; i < n; i++)
-            {
-                if (pixels[i][j] != pixels[n - i - 1][j])
-                {
-                    isLineSymmetry2 = false;
-                    break;
-                }
-            }
-        }
-
-        bool isLineSymmetry = isLineSymmetry1 || isLineSymmetry2;
-
-        // “_‘ÎÌ
-        bool isPointSymmetry = true;
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                if (pixels[i][j] != pixels[n - i - 1][m - j - 1])
-                {
-                    isPointSymmetry = false;
-                    break;
-                }
-            }
-        }
-
-        if (isLineSymmetry && isPointSymmetry)
-        {
-            Console.WriteLine("line point symmetry");
-        }
-        else if (isLineSymmetry)
-        {
-            Console.WriteLine("line symmetry");
-        }
-        else if (isPointSymmetry)
-        {
-            Console.WriteLine("point symmetry");
-        }
-        else
-        {
-            Console.WriteLine("none");
-        }
+        Console.WriteLine("Å‘åŒö–ñ”: " + gcd);
     }
 }
