2908b7a5d4b24f9e4abd28dbddfecef3e6f08964
Shinochi-Haru <haru060109011305@gmail.com>
2023-05-29 13:04:59 +0900

    修正

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -1,23 +1,42 @@
 using System;
-using System.Linq;
 
 class Program
 {
     static void Main()
     {
-        var input = Console.ReadLine().Split().Select(long.Parse).ToArray();
-        long gcd = GetGCD(input[0],input[1]);
-        
+        // 入力を受け取り、nとsに値を割り当てる
+        string[] input1 = Console.ReadLine().Split();
+        int n = int.Parse(input1[0]);
+        int s = int.Parse(input1[1]);
+
+        // n回のループで処理を行う
+        for (int i = 0; i < n; i++)
+        {
+            // vを受け取り、aに配列として格納する
+            int v = int.Parse(Console.ReadLine());
+            int[] a = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            // sとi+1が等しい場合、最大値を出力してループを終了する
+            if (s == i + 1)
+            {
+                int max = GetMaxValue(a);
+                Console.WriteLine(max);
+                break;
+            }
+        }
     }
 
-    static long GetGCD(long a, long b)
+    // 配列の最大値を求めるメソッド
+    static int GetMaxValue(int[] array)
     {
-        while (b != 0)
+        int max = array[0];
+        for (int i = 1; i < array.Length; i++)
         {
-            long temp = b;
-            b = a % b;
-            a = temp;
+            if (array[i] > max)
+            {
+                max = array[i];
+            }
         }
-        return a;
+        return max;
     }
 }
