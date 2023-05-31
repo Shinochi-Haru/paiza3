d590355b5674d29ee7f7c8d046f2e75646d41bc7
Shinochi-Haru <haru060109011305@gmail.com>
2023-05-31 14:37:33 +0900

    修正

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -1,67 +1,65 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
-class Program
+public class QuickSort
 {
-    static Dictionary<int, List<int>> adList;
-    static int[] minPath;
-    static int t;
-    static int p;
-    static void Main()
+    public static void Main()
     {
-        // 入力を受け取る
-        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
-        int n = input[0]; // 頂点の数
-        int s = input[1]; // 始点
-        t = input[2]; // 終点
-        p = input[3]; // p
+        int[] arr = { 7, 2, 1, 6, 8, 5, 3, 4 };
 
-        // 隣接リストを作成する
-        adList = new Dictionary<int, List<int>>();
-        for (int i = 1; i <= n; i++)
+        Console.WriteLine("元の配列: ");
+        PrintArray(arr);
+
+        QuickSortAlgorithm(arr, 0, arr.Length - 1);
+
+        Console.WriteLine("ソートされた配列: ");
+        PrintArray(arr);
+    }
+
+    private static void QuickSortAlgorithm(int[] arr, int low, int high)
+    {
+        if (low < high)
         {
-            int v = int.Parse(Console.ReadLine());
-            adList[i] = Console.ReadLine().Split().Select(int.Parse).ToList();
-        }
+            // 配列をパーティション分割する
+            int pivotIndex = Partition(arr, low, high);
 
-        // 最短パスを初期化する
-        minPath = Enumerable.Range(1, n + 1).ToArray();
-
-        // 深さ優先探索を実行する
-        DFS(s, new List<int> { s });
-
-        // 結果を出力する
-        if (minPath.Length == n + 1)
-        {
-            Console.WriteLine(-1);
-        }
-        else
-        {
-            Console.WriteLine(string.Join(" ", minPath));
+            // 2つのパーティションを再帰的にソートする
+            QuickSortAlgorithm(arr, low, pivotIndex - 1);
+            QuickSortAlgorithm(arr, pivotIndex + 1, high);
         }
     }
 
-    static void DFS(int v, List<int> path)
+    private static int Partition(int[] arr, int low, int high)
     {
-        if (path.Count < minPath.Length)
+        int pivot = arr[high]; // ピボットを選択（ここでは配列の最後の要素）
+
+        int i = low - 1; // iの初期値を設定
+
+        for (int j = low; j < high; j++)
         {
-            foreach (int i in adList[v])
+            if (arr[j] < pivot)
             {
-                if (!path.Contains(i))
-                {
-                    path.Add(i);
-                    if (i == t && path.Contains(p))
-                    {
-                        minPath = path.ToArray();
-                    }
-                    else
-                    {
-                        DFS(i, path);
-                    }
-                    path.RemoveAt(path.Count - 1);
-                }
+                i++;
+                Swap(arr, i, j); // 要素を交換する
             }
         }
+
+        Swap(arr, i + 1, high); // ピボットを正しい位置に移動する
+        return i + 1; // ピボットのインデックスを返す
+    }
+
+    private static void Swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private static void PrintArray(int[] arr)
+    {
+        foreach (int element in arr)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
     }
 }
