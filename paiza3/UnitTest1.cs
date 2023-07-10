a05e9e5238fbfa500ba3a29fa3e82a5896049784
Shinochi-Haru <haru060109011305@gmail.com>
2023-07-10 22:40:47 +0900

    syuuei

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -1,69 +1,51 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
-public class AdjacencyList
+class Program
 {
-    private List<List<int>> adjacencyList;
-    private int numVertices;
-
-    public static void Main()
+    static void Main()
     {
         string[] input = Console.ReadLine().Split();
-        int numVertices = int.Parse(input[0]);
-        int numEdges = int.Parse(input[1]);
+        int N = int.Parse(input[0]);
+        int M = int.Parse(input[1]);
 
-        AdjacencyList adjacencyList = new AdjacencyList(numVertices);
+        int[,] g = new int[N, N];
+        List<List<int>> G = new List<List<int>>();
 
-        for (int i = 0; i < numEdges; i++)
+        for (int i = 0; i < N; i++)
         {
-            string[] edgeInput = Console.ReadLine().Split();
-            int source = int.Parse(edgeInput[0]) - 1;
-            int destination = int.Parse(edgeInput[1]) - 1;
-
-            adjacencyList.AddEdge(source, destination);
+            G.Add(new List<int>());
         }
 
-        // 隣接リストの出力
-        for (int i = 0; i < numVertices; i++)
+        for (int i = 0; i < M; i++)
         {
-            foreach (int neighbor in adjacencyList.GetNeighbors(i))
+            input = Console.ReadLine().Split();
+            int a = int.Parse(input[0]) - 1;
+            int b = int.Parse(input[1]) - 1;
+            g[a, b] = 1;
+            G[a].Add(b);
+        }
+
+        Console.WriteLine("隣接行列:");
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
             {
-                Console.Write(neighbor + 1);
+                Console.Write(g[i, j]);
             }
             Console.WriteLine();
         }
-    }
 
-    public AdjacencyList(int numVertices)
-    {
-        this.numVertices = numVertices;
-        adjacencyList = new List<List<int>>(numVertices);
-
-        for (int i = 0; i < numVertices; i++)
+        Console.WriteLine("隣接リスト:");
+        for (int i = 0; i < N; i++)
         {
-            adjacencyList.Add(new List<int>());
+            G[i].Sort();
+            foreach (int neighbor in G[i])
+            {
+                Console.Write(neighbor + " ");
+            }
+            Console.WriteLine();
         }
-    }
-
-    public void AddEdge(int source, int destination)
-    {
-        adjacencyList[source].Add(destination);
-        adjacencyList[destination].Add(source);
-    }
-
-    public void RemoveEdge(int source, int destination)
-    {
-        adjacencyList[source].Remove(destination);
-        adjacencyList[destination].Remove(source);
-    }
-
-    public bool HasEdge(int source, int destination)
-    {
-        return adjacencyList[source].Contains(destination);
-    }
-
-    public List<int> GetNeighbors(int vertex)
-    {
-        return adjacencyList[vertex];
     }
 }
