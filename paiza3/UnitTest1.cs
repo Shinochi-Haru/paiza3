80ae3b8617af41046240eb794e61bf4360c2e1f3
Shinochi-Haru <haru060109011305@gmail.com>
2023-07-19 08:16:50 +0900

    深さ優先探索

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -1,38 +1,59 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
+    static Dictionary<int, List<int>> adjacencyList;
+
     static void Main()
     {
+        int n, k;
         string[] input = Console.ReadLine().Split(' ');
-        int n = int.Parse(input[0]);
-        int m = int.Parse(input[1]);
-        int k = int.Parse(input[2]);
+        n = int.Parse(input[0]);
+        k = int.Parse(input[1]);
 
-        List<int>[] adjacencyList = new List<int>[n + 1];
+        adjacencyList = new Dictionary<int, List<int>>();
+
+        // —×ÚƒŠƒXƒg‚Ìì¬
         for (int i = 1; i <= n; i++)
         {
-            adjacencyList[i] = new List<int>();
+            int v = int.Parse(Console.ReadLine());
+            adjacencyList[i] = new List<int>(Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse));
         }
 
-        for (int i = 0; i < m; i++)
-        {
-            string[] edge = Console.ReadLine().Split(' ');
-            int u = int.Parse(edge[0]);
-            int v = int.Parse(edge[1]);
-            adjacencyList[u].Add(v);
-            adjacencyList[v].Add(u);
-        }
+        HashSet<int> notVisited = new HashSet<int>(Enumerable.Range(1, n));
 
-        for (int i = 1; i <= n; i++)
+        // –¢–K–â‚Ì’¸“_‚ğ’Tõ‚µ‚Ä˜AŒ‹¬•ª‚ğ”»’è
+        while (notVisited.Count > 0)
         {
-            Console.Write($"Node {i}: ");
-            foreach (int neighbor in adjacencyList[i])
+            int v = notVisited.First();
+            List<int> connectedComp = DFS(v, new List<int>() { v });
+
+            if (connectedComp.Count > k)
             {
-                Console.Write($"{neighbor} ");
+                Console.WriteLine("No");
+                return;
             }
-            Console.WriteLine();
+
+            notVisited.ExceptWith(connectedComp);
         }
+
+        Console.WriteLine("Yes");
+    }
+
+    // [‚³—Dæ’TõiDFSj‚ğs‚¤ŠÖ”
+    static List<int> DFS(int v, List<int> connectedComp)
+    {
+        foreach (int i in adjacencyList[v])
+        {
+            if (!connectedComp.Contains(i))
+            {
+                connectedComp.Add(i);
+                DFS(i, connectedComp);
+            }
+        }
+
+        return connectedComp;
     }
 }
