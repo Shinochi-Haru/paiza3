37ed2646b2204ed4bfe68bf1df2e9aff8bf82f02
Shinochi-Haru <haru060109011305@gmail.com>
2023-07-18 14:32:44 +0900

    syuusei

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -1,36 +1,38 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        int n = int.Parse(Console.ReadLine());
+        string[] input = Console.ReadLine().Split(' ');
+        int n = int.Parse(input[0]);
+        int m = int.Parse(input[1]);
+        int k = int.Parse(input[2]);
 
-        bool hasTripleCrown = false;
-        bool hasDoubleCrown = false;
-
-        for (int i = 0; i < n; i++)
+        List<int>[] adjacencyList = new List<int>[n + 1];
+        for (int i = 1; i <= n; i++)
         {
-            string[] data = Console.ReadLine().Split(' ');
-            double battingAverage = double.Parse(data[0]);
-            int runsBattedIn = int.Parse(data[1]);
-            int homeRuns = int.Parse(data[2]);
-
-            if (battingAverage >= 0.3 && runsBattedIn >= 100 && homeRuns >= 30)
-                hasTripleCrown = true;
-            else if (battingAverage >= 0.3 && runsBattedIn >= 100)
-                hasDoubleCrown = true;
-            else if (battingAverage >= 0.3 && homeRuns >= 30)
-                hasDoubleCrown = true;
-            else if (runsBattedIn >= 100 && homeRuns >= 30)
-                hasDoubleCrown = true;
+            adjacencyList[i] = new List<int>();
         }
 
-        if (hasTripleCrown)
-            Console.WriteLine("Triple");
-        else if (hasDoubleCrown)
-            Console.WriteLine("Double");
-        else
-            Console.WriteLine("Nobody");
+        for (int i = 0; i < m; i++)
+        {
+            string[] edge = Console.ReadLine().Split(' ');
+            int u = int.Parse(edge[0]);
+            int v = int.Parse(edge[1]);
+            adjacencyList[u].Add(v);
+            adjacencyList[v].Add(u);
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write($"Node {i}: ");
+            foreach (int neighbor in adjacencyList[i])
+            {
+                Console.Write($"{neighbor} ");
+            }
+            Console.WriteLine();
+        }
     }
 }
