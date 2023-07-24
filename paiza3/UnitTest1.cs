339eaf0cc502f7c4f520731cd7b374e96156acbc
Shinochi-Haru <haru060109011305@gmail.com>
2023-07-24 19:47:37 +0900

    全域木の構築

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -1,47 +1,45 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-//双方向連結リスト
+
 class Program
 {
-    private static int[] numArray;
-    private static int count;
+    static Dictionary<int, List<int>> adjList; // 隣接リストを保持する辞書
+    static List<Tuple<int, int>> tree; // 全域木の辺を保持するリスト
 
-    private static void CustomList()
+    public static void Main(string[] args)
     {
-        numArray = new int[4];
-        count = 0;
-    }
+        int n = int.Parse(Console.ReadLine());
+        adjList = new Dictionary<int, List<int>>();
+        tree = new List<Tuple<int, int>>();
 
-    private static void AddList()
-    {
-        count++;
-        if(count == numArray.Length)
+        // 隣接リストを作成
+        for (int i = 1; i <= n; i++)
         {
-            int[] array = new int[count];
-            Array.Copy(numArray, array, numArray.Length);
-            numArray = array;
+            int v = int.Parse(Console.ReadLine()); // 隣接する頂点の数
+            adjList[i] = new List<int>(Array.ConvertAll(Console.ReadLine().Split(), int.Parse)); // 隣接する頂点の番号をリストとして保存
+        }
+
+        // 開始頂点を1としてDFSを実行し、全域木を構築する
+        DFS(1, new List<int>() { 1 });
+
+        // 全域木の辺を出力
+        foreach (var edge in tree)
+        {
+            Console.WriteLine($"{edge.Item1} {edge.Item2}");
         }
     }
 
-    private static void RemoveList()
+    // 深さ優先探索（DFS）を用いて全域木を構築する
+    public static void DFS(int v, List<int> visited)
     {
-        count--;
-        if(count == numArray.Length)
+        foreach (int i in adjList[v])
         {
-            int[] removeArray = new int[count];
-            Array.Copy(numArray, removeArray, numArray.Length);
-            numArray = removeArray;
+            if (!visited.Contains(i))
+            {
+                visited.Add(i); // 頂点iを訪れたことを記録
+                tree.Add(new Tuple<int, int>(v, i)); // 頂点vから頂点iへの辺を全域木に追加
+                DFS(i, visited); // 頂点iから再帰的にDFSを実行
+            }
         }
-    }
-
-    private static void SwapList()
-    {
-
-    }
-
-    private static void Sort()
-    {
-
     }
 }
