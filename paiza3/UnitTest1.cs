ccb9e1e891704398e5e001800cd6818acf71b6bd
Shinochi-Haru <haru060109011305@gmail.com>
2023-07-26 14:46:27 +0900

    隣接リストが与えられます。このとき、この隣接リストから枝を n-1 個選び、それぞれの頂点の次数が 2 以下となるような全域木を構成し、出力してください。頂点 v の次数とは頂点 v に接続されている枝の数のことを言います。そのような全域木が存在しない場合は -1 と出力してください。

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 class Program
 {
@@ -9,58 +8,47 @@ class Program
 
     public static void Main(string[] args)
     {
-        int n = int.Parse(Console.ReadLine()); // 頂点数
-        int k = int.Parse(Console.ReadLine()); // 削除する頂点数
-        HashSet<int> S = new HashSet<int>(Array.ConvertAll(Console.ReadLine().Split(), int.Parse)); // 削除する頂点の集合
+        int n = int.Parse(Console.ReadLine());
         adjList = new Dictionary<int, List<int>>();
         tree = new List<Tuple<int, int>>();
 
-        // 隣接リストを作成
         for (int i = 1; i <= n; i++)
         {
-            int v = int.Parse(Console.ReadLine()); // 隣接する頂点の数
-            adjList[i] = new List<int>(Array.ConvertAll(Console.ReadLine().Split(), int.Parse)); // 隣接する頂点の番号をリストとして保存
+            int v = int.Parse(Console.ReadLine());
+            adjList[i] = new List<int>(Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
         }
 
-        // 削除する辺を処理
-        foreach (int i in S)
+        // 開始頂点を各頂点としてDFSを実行し、全域木の辺を構築する
+        for (int i = 1; i <= n; i++)
         {
-            foreach (int j in adjList[i])
-            {
-                adjList[j].Remove(i); // 隣接する頂点から削除する頂点を削除
-            }
-            adjList[i].Clear(); // 削除する頂点を隣接リストから削除
+            DFS(i, new List<int>() { i });
         }
 
-        HashSet<int> vertices = new HashSet<int>(Enumerable.Range(1, n).Where(x => !S.Contains(x))); // 削除されていない頂点の集合
+        // 辺が見つからない場合は -1 を出力
+        Console.WriteLine("-1");
+    }
 
-        // 開始頂点を削除されていない頂点の中で最小のものとしてDFSを実行し、全域木を構築する
-        DFS(vertices.Min(), new List<int>() { vertices.Min() });
-
-        // 全域木の辺の数が n - k - 1 ならば全域木を出力、それ以外は -1 を出力
-        if (tree.Count == n - k - 1)
+    // 深さ優先探索（DFS）を用いて全域木の辺を構築する
+    public static void DFS(int v, List<int> visited)
+    {
+        if (visited.Count == adjList.Count)
         {
-            foreach (var edge in tree)
+            for (int i = 0; i < adjList.Count - 1; i++)
             {
-                Console.WriteLine($"{edge.Item1} {edge.Item2}");
+                Console.WriteLine($"{visited[i]} {visited[i + 1]}");
             }
+            Environment.Exit(0);
         }
         else
         {
-            Console.WriteLine("-1");
-        }
-    }
-
-    // 深さ優先探索（DFS）を用いて全域木を構築する
-    public static void DFS(int v, List<int> visited)
-    {
-        foreach (int i in adjList[v])
-        {
-            if (!visited.Contains(i))
+            foreach (int i in adjList[v])
             {
-                visited.Add(i); // 頂点iを訪れたことを記録
-                tree.Add(new Tuple<int, int>(v, i)); // 頂点vから頂点iへの辺を全域木に追加
-                DFS(i, visited); // 頂点iから再帰的にDFSを実行
+                if (!visited.Contains(i))
+                {
+                    visited.Add(i);
+                    DFS(i, visited);
+                    visited.Remove(i);
+                }
             }
         }
     }
