a24322b78c7457ddfc10e04db70abf9b54333e18
Shinochi-Haru <haru060109011305@gmail.com>
2023-09-03 00:29:21 +0900

    通貨

--- paiza3/UnitTest1.cs
+++ paiza3/UnitTest1.cs
@@ -1,11 +1,36 @@
 using System;
-using System.Linq;
 
 class Program
 {
     static void Main()
     {
-        var nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
-        Console.WriteLine(nums[0] - nums[1] + nums[2]);
+        int n = int.Parse(Console.ReadLine());
+        int[] m = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            m[i] = int.Parse(Console.ReadLine());
+        }
+
+        int maxProfit = 0;
+        int buyDay = 0;
+        int sellDay = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                int profit = m[j] - m[i];
+                if (profit > maxProfit)
+                {
+                    maxProfit = profit;
+                    buyDay = i + 1;  // 1-based index
+                    sellDay = j + 1; // 1-based index
+                }
+            }
+        }
+
+        Console.WriteLine(buyDay);
+        Console.WriteLine(sellDay);
     }
 }
